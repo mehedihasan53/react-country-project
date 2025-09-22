510d4ee34efcbc3b1f44c32db37fd1fede24326d
Mehedi Hasan <mehedihasan.codes3@gmail.com>
2025-09-23 00:45:05 +0600

    react-country-project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Countries from "./component/country/countries";
+
+function App() {
+  return (
+    <>
+      <Countries></Countries>
+    </>
+  );
+}
+
+export default App;
